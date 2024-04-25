fea33b3449fb2ccbc8254a38376e5f7a3801a659
Salih Kuloğlu <salihkuloglu@Salih-MacBook-Air.local>
2024-04-25 15:05:43 +0300

    header right box

--- src/companents/Header/index.jsx
+++ src/companents/Header/index.jsx
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState, useRef} from 'react'
 import classNames from 'classnames';
 
 
 const index = () => {
 
     const [isHide, setIsHide] = useState(true);
-    const [input, setInput] = useState("")
+    const [input, setInput] = useState("");
+    const inputRef = useRef(null);
 
     useEffect(()=>{
         const searchInput = document.getElementById('search-input');
@@ -73,9 +74,9 @@ const index = () => {
                             <div className={classNames('left-icon-box', {'hide-icon': isHide})}>
                                 <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="20" viewBox="0 0 24 24" width="20" focusable="false" style={{pointerEvents: "none", display: "inherit", width: "100%", height: "100%"}}><path d="m20.87 20.17-5.59-5.59C16.35 13.35 17 11.75 17 10c0-3.87-3.13-7-7-7s-7 3.13-7 7 3.13 7 7 7c1.75 0 3.35-.65 4.58-1.71l5.59 5.59.7-.71zM10 16c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6z"></path></svg>
                             </div>
-                            <input value={input}  onChange={(e)=>{setInput(e.target.value)}} type="text" placeholder='Ara' id='search-input' />
+                            <input ref={inputRef} value={input}  onChange={(e)=>{setInput(e.target.value)}} type="text" placeholder='Ara' id='search-input' />
                             <div className={classNames('search-clear-button', {'hide' : input.length <= 0})}>
-                                <button className='clear-button' onClick={()=>{setInput("")}}>
+                                <button className='clear-button' onClick={(e)=>{e.preventDefault(); inputRef.current.focus(); setInput("")}}>
                                     <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style={{pointerEvents: 'none', display: "inherit", width: "100%", height: "100%"}}><path d="m12.71 12 8.15 8.15-.71.71L12 12.71l-8.15 8.15-.71-.71L11.29 12 3.15 3.85l.71-.71L12 11.29l8.15-8.15.71.71L12.71 12z"></path></svg>
                                 </button>
                             </div>
@@ -109,7 +110,24 @@ const index = () => {
             </div>
 
             <div className='header__right-box'>
-                
+                <div className='buttons-box'>
+                    <div className='settings-button'>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24" focusable="false" style={{pointerEvents: "none", display: "inherit", width: "24px", height: "24px"}}><path d="M12 16.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zM10.5 12c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm0-6c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5z"></path></svg>
+                        </button>
+                    </div>
+
+                    <div className='login-button'>
+                        <div className='person-icon'>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style={{pointerEvents: "none", display: "inherit", width: "24px", height: "24px"}}><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 1c4.96 0 9 4.04 9 9 0 1.42-.34 2.76-.93 3.96-1.53-1.72-3.98-2.89-7.38-3.03A3.996 3.996 0 0016 9c0-2.21-1.79-4-4-4S8 6.79 8 9c0 1.97 1.43 3.6 3.31 3.93-3.4.14-5.85 1.31-7.38 3.03C3.34 14.76 3 13.42 3 12c0-4.96 4.04-9 9-9zM9 9c0-1.65 1.35-3 3-3s3 1.35 3 3-1.35 3-3 3-3-1.35-3-3zm3 12c-3.16 0-5.94-1.64-7.55-4.12C6.01 14.93 8.61 13.9 12 13.9c3.39 0 5.99 1.03 7.55 2.98C17.94 19.36 15.16 21 12 21z"></path></svg>
+                        </div>
+                        <div className='login-text'>
+                            <span className='text'>
+                                Oturum aç
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
