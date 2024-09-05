a1b4567621445224708218ffdbde4e9ab2ca11c0
Salih Kuloğlu <kuloglusalih10@gmail.com>
2024-09-05 14:44:44 +0300

    started to sidebar

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Sidebar from "./companents/Sidebar"
 
 
 const index = () => {
   return (
-    <div>
-        
+    <div className='main'>
+      <Sidebar/>
     </div>
   )
 }
